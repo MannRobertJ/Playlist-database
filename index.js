c18080511158741ba92b85f1209abc42a382aa60
MannRobertJ <mann.robertj@gmail.com>
2019-02-09 09:18:44 +0100

    added playlists model and get route

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const usersRouter = require("./users/routes");
+const playlistsRouter = require("./playlists/routes");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app
   .use(bodyParser.json())
-  .use(usersRouter)
+  .use(usersRouter, playlistsRouter)
   .listen(port, () => console.log(`Listening on port ${port}`));
