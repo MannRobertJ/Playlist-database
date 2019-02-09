39282d59e5e16ba64e93158341be4582b6a5d7a6
MannRobertJ <mann.robertj@gmail.com>
2019-02-09 17:15:45 +0100

    songs can now be edited

--- songs/routes.js
+++ songs/routes.js
@@ -48,6 +48,84 @@ router.post("/playlists/:id/songs", (req, res, next) => {
   }
 });
 
+router.put("/playlists/:playlistId/songs/:songId", (req, res, next) => {
+  const auth =
+    req.headers.authorization && req.headers.authorization.split(" ");
+
+  newPlayListId = req.body.PlaylistId
+    ? req.body.newPlaylistId
+    : req.params.playlistId;
+
+  const moveSongToNewPlaylist = (song, playlist) => {
+    return song
+      .update(req.body)
+      .then(song => res.send({ message: `Song edited` }));
+  };
+
+  const findNewPlayList = (userId, playlist, song, newPlaylistId) => {
+    Playlist.findOne({ userId: userId, id: newPlaylistId })
+      .then(newPlaylist => {
+        if (!newPlaylist) {
+          return res.status(404).send({ message: `Song does not exist` });
+        }
+        moveSongToNewPlaylist(song, newPlaylist);
+      })
+      .catch(error => next(error));
+  };
+
+  const findCurrentPlaylist = (userId, playlistId, song) => {
+    Playlist.findOne({ userId: userId, id: playlistId })
+      .then(playlist => {
+        if (
+          !playlist ||
+          playlist.id !== song.playlistId ||
+          playlist.userId !== userId
+        ) {
+          return res.status(404).send({
+            message: `Song does not exist`
+          });
+        }
+        findNewPlayList(userId, playlist, song, newPlayListId);
+      })
+      .catch(error => next(error));
+  };
+
+  const findSong = (userId, playlistId, songId) => {
+    Song.findById(songId)
+      .then(song => {
+        if (!song || song.playlistId !== playlistId) {
+          return res.status(404).send({
+            message: `Song does not exist`
+          });
+        }
+        findCurrentPlaylist(userId, playlistId, song);
+      })
+      .catch(error => next(error));
+  };
+
+  if (auth && auth[0] === "Bearer" && auth[1]) {
+    try {
+      const data = toData(auth[1]);
+      findSong(
+        Number(data.userId),
+        Number(req.params.playlistId),
+        Number(req.params.songId)
+      );
+    } catch (error) {
+      res.status(400).send({
+        Error: `Error ${error.name}: ${error.message}`
+      });
+    }
+  } else {
+    res
+      .status(401)
+      .send({
+        message: "Please supply some valid credentials"
+      })
+      .catch(error => next(error));
+  }
+});
+
 router.delete("/playlists/:playlistId/songs/:songId", (req, res, next) => {
   const auth =
     req.headers.authorization && req.headers.authorization.split(" ");
@@ -60,9 +138,7 @@ router.delete("/playlists/:playlistId/songs/:songId", (req, res, next) => {
         playlist.userId !== userId
       ) {
         return res.status(404).send({
-          message: `${!playlist} ||
-        ${playlist.id !== song.playlistId} ||
-        ${playlist.userId !== userId} ${playlist.id} ${song.playlistId}`
+          message: `Song does not exist`
         });
       }
       song.destroy().then(() => res.send({ message: `Song was destroyed` }));
@@ -81,7 +157,7 @@ router.delete("/playlists/:playlistId/songs/:songId", (req, res, next) => {
   if (auth && auth[0] === "Bearer" && auth[1]) {
     try {
       const data = toData(auth[1]);
-      findSong(Number(data.userId, req.params.playlistId, req.params.songId));
+      findSong(Number(data.userId), req.params.playlistId, req.params.songId);
     } catch (error) {
       res.status(400).send({
         Error: `Error ${error.name}: ${error.message}`
