cc90a2309b75afbd504537b5356d77d2ae23a803
MannRobertJ <mann.robertj@gmail.com>
2019-02-09 21:41:55 +0100

    failed to implement middleware

--- auth/routes.js
+++ auth/routes.js
@@ -1,10 +1,11 @@
+const { auth } = require("./middleware");
 const { Router } = require("express");
 const User = require("../users/model");
 const router = new Router();
 const bcrypt = require("bcrypt");
 const { toJWT, toData } = require("./jwt");
 
-router.post("/logins", (req, res, next) => {
+router.post("/logins", (req, res) => {
   User.findOne({
     where: {
       email: req.body.email
@@ -34,27 +35,12 @@ router.post("/logins", (req, res, next) => {
     });
 });
 
-// Not part of assignment but helpful reference for implementing authorisation.
-router.get("/secret-endpoint", (req, res) => {
-  const auth =
-    req.headers.authorization && req.headers.authorization.split(" ");
-  if (auth && auth[0] === "Bearer" && auth[1]) {
-    try {
-      const data = toData(auth[1]);
-      res.send({
-        message: "Thanks for visiting the secret endpoint.",
-        data
-      });
-    } catch (error) {
-      res.status(400).send({
-        message: `Error ${error.name}: ${error.message}`
-      });
-    }
-  } else {
-    res.status(401).send({
-      message: "Please supply some valid credentials"
-    });
-  }
-});
+// When I try to implement the middleware as explained in the reader it breaks.
+
+/* router.get("/secret-endpoint", auth, (req, res) => {
+  res.send({
+    message: `Thanks for visiting the secret endpoint ${req.user.email}.`
+  });
+}); */
 
 module.exports = router;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const usersRouter = require("./users/routes");
 const playlistsRouter = require("./playlists/routes");
 const songsRouter = require("./songs/routes");
 const authRouter = require("./auth/routes");
-
+const auth = require("./auth/middleware");
 const app = express();
 const port = process.env.PORT || 4000;
 
